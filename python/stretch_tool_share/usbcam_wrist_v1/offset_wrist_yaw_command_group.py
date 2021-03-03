05b0021cc82d449c342ee8ea29ea93c978ddfcc0
hello-binit <64861565+hello-binit@users.noreply.github.com>
2021-03-03 10:19:58 -0500

    Implement joint state info for usbcam command group

--- python/stretch_tool_share/usbcam_wrist_v1/offset_wrist_yaw_command_group.py
+++ python/stretch_tool_share/usbcam_wrist_v1/offset_wrist_yaw_command_group.py
@@ -146,6 +146,21 @@ class SimpleCommandGroup:
         """
         raise NotImplementedError
 
+    def joint_state(self, robot_status, **kwargs):
+        """Returns joint state of the robot
+
+        Parameters
+        ----------
+        robot_status: dict
+            robot's current status
+
+        Returns
+        -------
+        (float, float, float)
+            Current position, velocity, and effort
+        """
+        raise NotImplementedError
+
     def goal_reached(self):
         """Returns whether reached target point
 
@@ -162,15 +177,16 @@ class SimpleCommandGroup:
 
 class OffsetWristYawCommandGroup(SimpleCommandGroup):
     def __init__(self, range_rad, robot=None):
-        if range_rad is None and robot is not None:
-            range_ticks = robot.end_of_arm.motors['wrist_yaw'].params['range_t']
-            range_rad = (robot.end_of_arm.motors['wrist_yaw'].ticks_to_world_rad(range_ticks[1]),
-                         robot.end_of_arm.motors['wrist_yaw'].ticks_to_world_rad(range_ticks[0]))
-        SimpleCommandGroup.__init__(self, 'joint_wrist_yaw', range_rad)
+        if (range_rad is None and robot is not None and robot.end_of_arm is not None
+            and robot.end_of_arm.is_tool_present('OffsetWristYaw')):
+            range_ticks = robot.end_of_arm.motors['offset_wrist_yaw'].params['range_t']
+            range_rad = (robot.end_of_arm.motors['offset_wrist_yaw'].ticks_to_world_rad(range_ticks[1]),
+                         robot.end_of_arm.motors['offset_wrist_yaw'].ticks_to_world_rad(range_ticks[0]))
+        SimpleCommandGroup.__init__(self, 'joint_offset_wrist_yaw', range_rad)
 
     def init_execution(self, robot, robot_status, **kwargs):
         if self.active:
-            robot.end_of_arm.move_by('wrist_yaw',
+            robot.end_of_arm.move_by('offset_wrist_yaw',
                                      self.update_execution(robot_status)[1],
                                      v_r=self.goal['velocity'],
                                      a_r=self.goal['acceleration'])
@@ -178,7 +194,11 @@ class OffsetWristYawCommandGroup(SimpleCommandGroup):
     def update_execution(self, robot_status, **kwargs):
         self.error = None
         if self.active:
-            self.error = self.goal['position'] - robot_status['end_of_arm']['wrist_yaw']['pos']
+            self.error = self.goal['position'] - robot_status['end_of_arm']['offset_wrist_yaw']['pos']
             return self.name, self.error
 
         return None
+
+    def joint_state(self, robot_status, **kwargs):
+        yaw_status = robot_status['end_of_arm']['offset_wrist_yaw']
+        return (yaw_status['pos'], yaw_status['vel'], yaw_status['effort'])
