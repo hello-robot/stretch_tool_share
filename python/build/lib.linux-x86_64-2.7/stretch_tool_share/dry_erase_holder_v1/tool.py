f60b9200745be91ea47206dda7481ae49c0fac19
Aaron Edsinger <aedsinger@hello-robot.com>
2021-06-20 00:14:49 -0700

    remove build

--- python/build/lib.linux-x86_64-2.7/stretch_tool_share/dry_erase_holder_v1/tool.py
+++
@@ -1,5 +0,0 @@
-from stretch_body.end_of_arm import EndOfArm
-
-class ToolDryEraseHolderV1(EndOfArm):
-    def __init__(self, name='tool_dry_erase_holder_v1'):
-        EndOfArm.__init__(self, name)
